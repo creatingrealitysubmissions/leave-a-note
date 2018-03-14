c9e6702d20fab973ca9af47807157dad280ffb28
Po-Hsun Chen <godpopo34@gmail.com>
2018-03-14 09:35:43 -0700

    CameraSign
    Add a UI that shows the user position

---
+++ LeaveANote/Assets/Script/GeoTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeoTracker : MonoBehaviour {
+
+	public GameObject LocationSign;
+	public float m_Lat, m_Lon;
+
+	// Use this for initialization
+	void Start () {
+		LocationSign = GameObject.Find ("UI/Location");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate(){
+		LocationSign.GetComponent<Text> ().text = "Camera Location: " + Camera.main.transform.position.x + ", " + Camera.main.transform.position.y + ", " + Camera.main.transform.position.z;
+	}
+
+	public void GeneratePoint(){
+		
+	}
+}
